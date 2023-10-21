ccd644a180928aa9cdaf1857f34798587ac1b8be
VasKD <kiara.denisse96@gmail.com>
2023-10-21 13:51:17 -0400

    Tables created

--- system.py
+++ system.py
@@ -1,36 +1,62 @@
 import psycopg2
 
-conn = psycopg2.connect(database = "TheHiddenLeaf", 
-                        user = "postgres", 
-                        host= 'localhost',
-                        password = "postgres",
-                        port = 5432)
+class System:
+    # connect to local db 
+    conn = psycopg2.connect(database = "TheHiddenLeaf", 
+                            user = "postgres", 
+                            host= 'localhost',
+                            password = "postgres",
+                            port = 5432)
 
-# Open a cursor to perform database operations
-cur = conn.cursor()
-# # Execute a command: create datacamp_courses table
-# cur.execute("""CREATE TABLE datacamp_courses(
-#             course_id SERIAL PRIMARY KEY,
-#             course_name VARCHAR (50) UNIQUE NOT NULL,
-#             course_instructor VARCHAR (100) NOT NULL,
-#             topic VARCHAR (20) NOT NULL);
-#             """)
-# # Make the changes to the database persistent
-# conn.commit()
+    # Open a cursor to perform database operations
+    cur = conn.cursor()
+    
+    # create plants table and commit to db
+    plants = """CREATE TABLE plants(
+        plantID CHAR(3) PRIMARY KEY,
+        name VARCHAR(25),
+        price NUMERIC(2, 2),
+        discount NUMERIC(3, 2) DEFAULT NULL,
+        qty INTEGER,
+        type VARCHAR(25),
+        species VARCHAR(50),
+        duration VARCHAR(15),
+        description TEXT); """
 
+    cur.execute(plants)
 
-# cur.execute("INSERT INTO datacamp_courses(course_name, course_instructor, topic) VALUES('Introduction to SQL','Izzy Weber','Julia')");
+    # create plantcare table and commit to db
+    plantCare = """CREATE TABLE plantCare(
+        careID CHAR(3) PRIMARY KEY,
+        plantID CHAR(3) REFERENCES Plants ON DELETE CASCADE,
+        maintenance VARCHAR(10),
+        sunlight VARCHAR(20),
+        water INTEGER);"""
+    
+    cur.execute(plantCare)
 
-# cur.execute("INSERT INTO datacamp_courses(course_name, course_instructor, topic) VALUES('Analyzing Survey Data in Python','EbunOluwa Andrew','Python')");
+    # create customers table and commit to db
+    customers = """CREATE TABLE customers(
+        username VARCHAR(50) PRIMARY KEY,
+        password VARCHAR(8),
+        fname VARCHAR(50),
+        lname VARCHAR(50),
+        email VARCHAR(100),
+        phone CHAR(10) DEFAULT NULL);"""
 
-# cur.execute("INSERT INTO datacamp_courses(course_name, course_instructor, topic) VALUES('Introduction to ChatGPT','James Chapman','Theory')");
+    cur.execute(customers)  
 
-# cur.execute("INSERT INTO datacamp_courses(course_name, course_instructor, topic) VALUES('Introduction to Statistics in R','Maggie Matsui','R')");
+    # create orders table and commit to db
+    orders = """CREATE TABLE orders(
+        orderID CHAR(4) PRIMARY KEY,
+        plantID CHAR(3) REFERENCES plants,
+        username VARCHAR(50) REFERENCES customers,
+        qty INTEGER,
+        date TIMESTAMP(0));"""
 
-# cur.execute("INSERT INTO datacamp_courses(course_name, course_instructor, topic) VALUES('Hypothesis Testing in Python','James Chapman','Python')");
+    cur.execute(orders)
 
-cur.execute("DROP TABLE datacamp_courses");
-
-conn.commit()
-cur.close()
-conn.close()
+    # close connection with db
+    conn.commit()
+    cur.close()
+    conn.close()
