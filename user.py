b297bf2720684f4bbc8901c55ec1ea3008e358cf
VasKD <kiara.denisse96@gmail.com>
2023-10-26 15:02:05 -0400

    Updated login function

--- user.py
+++ user.py
@@ -1,14 +1,20 @@
 class User:
-    def __init__(self, username, password, fname, lname, email, phone=None):
+    def __init__(self, username, password, fname, lname, email, phone=None, loggedIn=False):
         self.username = username
         self.password = password 
         self.fname = fname
         self.lname = lname 
         self.email = email 
         self.phone = phone
+        self.loggedIn = loggedIn
 
-    def login(self):
-        pass
+    def login(self, username, fname, lname, email):
+        self.username = username
+        self.fname = fname
+        self.lname = lname
+        self.email = email
+        self.loggedIn = True
+        
 
     def logout(self):
         pass
