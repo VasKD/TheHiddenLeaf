553d0b8a00dc5925cee8c59993316b77749e7162
VasKD <kiara.denisse96@gmail.com>
2023-10-27 18:20:17 -0400

    Users can now signup and login 1. Login, Signup and Authentication functions completed 2. New validation functions: Username, Password, Name, Email, Phone

--- system.py
+++ system.py
@@ -1,6 +1,7 @@
+import re
 import psycopg2
-import os, sys, user
-
+import os, sys
+from user import User
 
 class System:
     def __init__(self):
@@ -8,7 +9,7 @@ class System:
                     "Login":self.login,
                     "Sign Up":self.signUp,
                     "Browse For Plants":self.browse,
-                    "Exit": self.leaf
+                    "Exit":self.leaf
         }
 
         # connect to local db 
@@ -49,8 +50,8 @@ class System:
 
         # create customers table and commit to db
         customers = """CREATE TABLE IF NOT EXISTS customers(
-            username VARCHAR(50) PRIMARY KEY,
-            password VARCHAR(8),
+            username VARCHAR(15) PRIMARY KEY,
+            password VARCHAR(12),
             fname VARCHAR(50),
             lname VARCHAR(50),
             email VARCHAR(100),
@@ -70,6 +71,10 @@ class System:
 
         # close connection with db -- needs to close when user logs off
         self.conn.commit()
+    
+        # create an instance of a user object
+        self.user = User("guest", "", "", "", False)
+
    
     # deconstructor for closing the db connection
     def __del__(self):
@@ -172,30 +177,131 @@ class System:
     # function for the Login page
     def login(self):
         self.printTitle("Login")
-        self.comingSoon(self.start)
-
-        # username = input("\nEnter Your Username: ")
-        # password = input("\nEnter Your Password: ")
-        # validLogin = self.authentication(username, password)
-        # if validLogin:
-        #     pass
-        # else:
-        #     self.clearConsole()
-        #     print("\nPlease Enter a Valid Username and Password: \n")
-        #     self.login()
+        username = input("Enter Your Username: ")
+        password = input("Enter Your Password: ")
+        validLogin, customer = self.authentication(username, password)
+        # if the login is valid, set user object attributes to what's stored in the db
+        if validLogin:
+            self.user.login(username=customer[0],
+                            fname=customer[2],
+                            lname=customer[3],
+                            email=customer[4],
+                            phone=customer[5])
+            # bring user to start page
+            return self.start()
+        else:
+            # prompt user to try again
+            self.clearConsole()
+            print("\nPlease Enter a Valid Username and Password: \n")
+            self.login()
     
 
-    def authentication(self):
+    # function to authenticate user credentials
+    def authentication(self, username, password):
         isValid = False
         # Insert authentication steps here
-        return isValid
+        self.cur.execute('SELECT username, password, fname, lname, email, phone FROM customers WHERE username = %s AND password = %s', (username, password))
+        customer = self.cur.fetchone()
+        if customer:
+            isValid = True
+        return isValid, customer
 
+    # function to validate username
+    def validateUsername(self, username):
+        # check length of username
+        if len(username) < 1 or len(username) > 15:
+            print("\nUsername must be 1-15 Characters Long")
+            return False 
+        # check if username exists in the db
+        self.cur.execute('SELECT * FROM Customers WHERE username=%s;', (username,))
+        userExists = self.cur.fetchone()
+        if userExists:
+            print("Username Already Exists")
+            return False
+        return True
+
+
+    # function that checks the password is valid
+    def validatePassword(self, password, confirmPass):
+        # check if the passwords are the same
+        if password != confirmPass:
+            print("\nPlease Ensure the Passwords Match")
+            return False
+        # check length of password
+        if len(password) < 8 or len(password) > 12:
+            print("\nPassword Must Be 8-12 Characters Long")
+            return False
+        # check if it has a digit
+        if not any(char.isdigit() for char in password):
+            print("\nPassword Must Contain At Least One Digit")
+            return False 
+        if not any(char.isupper() for char in password):
+            print("\nPassword Must Contain At Least One Capital Letter")
+            return False
+        if not re.search('[@_!#$%^&*()<>?|}{~:]', password):
+            print("\nPassword Must Contain At Least One Special Character")
+            return False
+        return True
+
+    
+    # function to validate first and last name
+    def validateName(self, fname, lname):
+        if len(fname) < 1 or len(fname) > 50:
+            print("\nFirst Name Must be 1-50 Characters Long")
+            return False
+        if len(lname) < 1 or len(lname) > 50:
+            print("\nLast Name Must be 1-50 Characters Long")
+            return False
+        return True
+
+    # function to validate email
+    def validateEmail(self, email):
+        regex = r'\b[a-zA-Z0-9._]+@[a-zA-Z0-9-.]+.[A-Za-z]\b'
+        if not re.match(regex, email):
+            print("\nPlease Enter A Valid Email Address")
+            return False
+        return True
+
+    # function to valoidate phone number
+    def validatePhone(self, phone):
+        if phone == "":
+            return True
+        if not phone.isdigit():
+            print("\nPhone Number Must Contain 10 Digits")
+            return False
+        if len(phone) < 10 or len(phone) > 10:
+            print("\nPhone Number Must Contain 10 Digits")
+            return False
 
     # function for the Sign Up page
     def signUp(self):
         self.printTitle("Sign Up")
-        self.comingSoon(self.start)
-
+        # prompt user to enter necessary info
+        username = input("\nEnter Your Username: ")
+        password = input("Enter Your Password: ")
+        confirmPass = input("Confirm Password: ")
+        fname = input("\nEnter First Name: ")
+        lname = input("Enter Last Name: ")
+        email = input("Enter Email Address: ")
+        phone = input("Enter Phone Number (optional): ")
+        
+        # validate user input
+        validUsername = self.validateUsername(username)
+        validPass = self.validatePassword(password, confirmPass)
+        validEmail = self.validateEmail(email)
+        validPhone = self.validatePhone(phone)
+        
+        # save the account to the db
+        if validUsername and validPass and validEmail and validPhone:
+            self.cur.execute("INSERT INTO Customers (username, password, fname, lname, email, phone) VALUES (%s, %s, %s, %s, %s, %s)", (username, password, fname, lname, email, phone))
+            self.conn.commit()
+            # bring user to login screen
+            self.clearConsole() 
+            return self.login()
+        else: 
+            print("\nAccount Creation Failed\n")
+            return
+        
 
     # function for browsing the plants (as a guest)
     def browse(self):
@@ -207,10 +313,11 @@ class System:
     def start(self):
         self.clearConsole()
         self.printTitle("The Hidden Leaf")
-        if User.loggedIn:
+        if self.user.loggedIn:
             options = ["Browse For Plants", "Exit"]
+            self.optionSelection(options)
         else:
             options = ["Login", "Sign Up", "Browse For Plants", "Exit"]
-        self.optionSelection(options)
+            self.optionSelection(options)
 
     
